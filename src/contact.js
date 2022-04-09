46bda4ab6f87eb91381f3134817ffa443f081d12
sohamtembhurne <sohamatembhurne@outlook.com>
2022-04-10 01:10:00 +0530

    Attempt to add firebase

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,45 @@
 import './contact.css';
+import { useState } from "react";
 
-function Contact() {
+const Contact = () => {
+    const [userData, setUserData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+    });
+
+
+    let name, value;
+    const postUserData = (event) => {
+        name = event.target.name;
+        value = event.target.value;
+
+        setUserData({ ...userData, [name]: value })
+    };
+
+    const submitData = async (e) => {
+        e.preventdefault();
+        const { name, email, subject, message } = userData;
+        const res = fetch(
+            "https://contact-form-5db31-default-rtdb.firebaseio.com/users.json",
+            {
+                method: "POST",
+                headers: {
+                    "Content:Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name, email, subject, message
+                }),
+            }
+        );
+        if (res) {
+            alert("Data stored, we will contact you soon");
+        } else {
+            alert("Please fill the form completely");
+        }
+    };
+
     return (
         <div className="mainb">
             <br></br>
@@ -10,38 +49,53 @@ function Contact() {
                     color: "rgba(0, 20, 39, 0.75)",
                     marginTop: "-9px",
                     marginBottom: "10vh",
-                }} data-aos="fade-up"
+                }}
             >Leave a message to contact our team
             </p>
             <div className="form_con">
-                <form>
+                <form method="POST">
                     <div className="col-1" style={{ display: "inline-block" }}>
                         <fieldset>
                             <input
+                                name="name"
+                                type="text"
                                 className="input_con"
                                 placeholder="Name"
+                                value={userData.name}
+                                onChange={postUserData}
                             ></input>
                             <input
+                                name="email"
+                                type="text"
                                 className="input_con"
                                 placeholder="Email"
+                                value={userData.email}
+                                onChange={postUserData}
                             ></input>
                             <input
+                                name="subject"
+                                type="text"
                                 className="input_con"
                                 placeholder="Subject"
+                                value={userData.subject}
+                                onChange={postUserData}
                             ></input>
                         </fieldset>
                     </div>
                     <div className="col-2" style={{ display: "inline-block" }}>
                         <fieldset>
                             <textarea
+                                name="message"
                                 cols="40"
                                 className="textarea_con"
                                 placeholder="Message"
+                                value={userData.message}
+                                onChange={postUserData}
                             ></textarea>
                         </fieldset>
                     </div>
+                    <button type="submit" onClick={submitData} >Send Message</button>
                 </form>
-                <button>Send Message</button>
             </div>
             <br></br>
         </div>
